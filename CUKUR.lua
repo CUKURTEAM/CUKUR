8d3326fa1660b8c1980be1a22139b05d3e7f05d8
CUKURTEAM <88411506+CUKURTEAM@users.noreply.github.com>
2021-08-08 04:22:26 +0300

    Add files via upload

--- CUKUR.lua
+++ CUKUR.lua
@@ -152,7 +152,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/CUKURLTEAM/Files_CUKUR/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/CUKURTEAM/Files_CUKUR/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1067,7 +1067,7 @@ end
 
 if text == 'تحديث السورس☆' and Devcukur(msg) then 
 os.execute('rm -rf CUKUR.lua')
-os.execute('wget https://raw.githubusercontent.com/CUKURLTEAM/CUKUR/main/CUKUR.lua')
+os.execute('wget https://raw.githubusercontent.com/CUKURTEAM/CUKUR/main/CUKUR.lua')
 send(msg.chat_id_, msg.id_,'☆︙ تم تحديث السورس\n☆︙ لديك اخر اصدار لسورس الحفره\n☆︙ الاصدار » { v 1.6 }')
 dofile('CUKUR.lua')  
 end
@@ -2364,7 +2364,7 @@ end
 return false
 end
 os.execute('rm -rf CUKUR.lua')
-os.execute('wget https://raw.githubusercontent.com/CUKURLTEAM/CUKUR/main/CUKUR.lua')
+os.execute('wget https://raw.githubusercontent.com/CUKURTEAM/CUKUR/main/CUKUR.lua')
 send(msg.chat_id_, msg.id_,'☆︙ تم تحديث السورس \n☆︙ لديك اخر اصدار لسورس الحفره\n☆︙ الاصدار » { v 1.6 }')
 dofile('CUKUR.lua')  
 end
@@ -4046,7 +4046,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if Devcukur(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/CUKURLTEAM/Files_CUKUR/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/CUKURTEAM/Files_CUKUR/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -4084,7 +4084,7 @@ t = " ☆∫ الملف » "..file.."\n ☆∫ تم تعطيل ملف \n"
 else
 t = " ☆∫ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/CUKURLTEAM/Files_CUKUR/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/CUKURTEAM/Files_CUKUR/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -4104,7 +4104,7 @@ t = " ☆∫ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ☆∫ الملف » "..file.."\n ☆∫ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/CUKURLTEAM/Files_CUKUR/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/CUKURTEAM/Files_CUKUR/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
